2048d58ea309eafde6e96a4df85b9ba26de5547e
Dung1109 <124704925+Dung1109@users.noreply.github.com>
2024-06-01 00:56:20 +0000

    add nextjs

---
+++ my-app/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold">Home Page</h1>
+      {/* <pre>{JSON.stringify(session, null, 2)}</pre> */}
+    </main>
+  );
+}
